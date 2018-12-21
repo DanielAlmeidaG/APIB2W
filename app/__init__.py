cd1f35c08c400041f32f113c306ff35c3194c7a0
Daniel Garrido <danieldagarrido@gmail.com>
2018-12-21 15:37:40 -0200

    Alterações em pequenos detalhes e nome de BD

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_restless import APIManager
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/storage.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/storagePlanet.db'
 db = SQLAlchemy(app) 
 
 manager = APIManager(app, flask_sqlalchemy_db=db)
